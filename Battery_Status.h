2014f6b7041d0a70af0441613ddfcaf515b80c1c
vekash2021 <106808639+vekash2021@users.noreply.github.com>
2024-07-24 19:39:22 +0530

    Create Battery_Status.h

---
+++ Battery_Status.h
@@ -0,0 +1,29 @@
+#ifndef BATTERY_STATUS_H
+#define BATTERY_STATUS_H
+
+typedef enum
+{
+  NORMAL_SOC_STATUS,
+  APPROACH_CHARGE,
+  APPROACH_DISCHARGE,
+  OUT_OF_RANGE_SOC
+}BattStatus_SoC_Chk_t;
+
+typedef enum
+{
+  NORMAL_TEMP_STATUS,
+  OUT_OF_RANGE_TEMP
+}BattStatus_Temp_Chk_t;
+
+typedef enum
+{
+  NORMAL_CHARGERATE,
+  OUT_OF_RANGE_CHARGERATE
+}BattStatus_ChargeRate_Chk_t;
+
+BattStatus_SoC_Chk_t socIsOk(float soc);
+BattStatus_Temp_Chk_t TemperatureIsOk(float temperature);
+BattStatus_ChargeRate_Chk_t chargeRateIsOk(float chargeRate);
+
+
+#endif BATTERY_STATUS_H
