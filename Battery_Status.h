34ff03e3e8b47d336b1f17c28a9230702d5028aa
vekash2021 <106808639+vekash2021@users.noreply.github.com>
2024-07-28 14:37:04 +0530

    Update Battery_Status.h

--- Battery_Status.h
+++ Battery_Status.h
@@ -1,29 +1,37 @@
 #ifndef BATTERY_STATUS_H
 #define BATTERY_STATUS_H
 
-typedef enum
-{
-  NORMAL_SOC_STATUS,
-  APPROACH_CHARGE,
-  APPROACH_DISCHARGE,
-  OUT_OF_RANGE_SOC
-}BattStatus_SoC_Chk_t;
+// Global language variable
+enum Language { EN, DE };
+enum Language language = 1;  // Default to English
 
-typedef enum
-{
-  NORMAL_TEMP_STATUS,
-  OUT_OF_RANGE_TEMP
-}BattStatus_Temp_Chk_t;
+// Messages in both languages
+const char *messages[2][5] = {
+    // EN
+    {
+        "Normal",
+        "Warning: Approaching discharge",
+        "Warning: Approaching charge-peak",
+        "Alarm: Low limit breached",
+        "Alarm: High limit breached"
+    },
+    // DE
+    {
+        "Normal",
+        "Warnung: Entladung nähert sich",
+        "Warnung: Ladehöhe nähert sich",
+        "Alarm: Untergrenze überschritten",
+        "Alarm: Obergrenze überschritten"
+    }
+};
 
-typedef enum
-{
-  NORMAL_CHARGERATE,
-  OUT_OF_RANGE_CHARGERATE
-}BattStatus_ChargeRate_Chk_t;
-
-BattStatus_SoC_Chk_t socIsOk(float soc);
-BattStatus_Temp_Chk_t TemperatureIsOk(float temperature);
-BattStatus_ChargeRate_Chk_t chargeRateIsOk(float chargeRate);
+// Configuration for parameter limits and warning levels
+typedef struct {
+    float min;
+    float max;
+    float warning_tolerance;
+    bool enable_warning;
+} ParameterConfig;
 
 
 #endif /*BATTERY_STATUS_H */
